0c799cc1631f5852fb3468f89931dcd053e206d7
Takahiro Sakamoto <e165723@ie.u-ryukyu.ac.jp>
2016-12-07 03:38:47 +0900

    report5の例外について

--- src/main/main.java
+++ src/main/main.java
@@ -2,11 +2,15 @@
  * Created by e165723 on 2016/12/07.
  */
 public class main {
-    public static void main(String[] args){
-        String str ;
-        int len;
-        str = null;
-        len = str.length();
-        System.out.println(len);
+    public static void main(String[] args) {
+        try {
+            String str;
+            int len;
+            str = null;
+            len = str.length();
+            System.out.println(len);
+        } catch (NullPointerException e) {
+          
+        }
     }
 }
