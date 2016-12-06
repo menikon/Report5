c82374de49e0229bcfbf646277af71d9227ca042
Takahiro Sakamoto <e165723@ie.u-ryukyu.ac.jp>
2016-12-07 03:24:39 +0900

    report5の例外について

---
+++ src/main/main.java
@@ -0,0 +1,12 @@
+/**
+ * Created by e165723 on 2016/12/07.
+ */
+public class main {
+    public static void main(String[] args){
+        String str ;
+        int len;
+        str = null;
+        len = str.length();
+        System.out.println(len);
+    }
+}
